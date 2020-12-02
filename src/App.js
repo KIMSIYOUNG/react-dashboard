912f407946d78d9fb9d79db9bb83c22bb09dbc12
KIMSIYOUNG <tldud2404@gmail.com>
2020-12-02 15:50:36 +0900

    feat : side bar

--- src/App.js
+++ src/App.js
@@ -22,26 +22,91 @@ const AppContainer = styled.div`
 const SHeader = styled.div`
   display: flex;
   height: 80px;  
-  background-color:blue;
+  background-color:#00cec9;
 `
 
-const MenuButton = styled.div`
+const MenuContainer = styled.div`
   width: 80px;
   height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const IconButton = styled.div`
+  background: ${({url}) => "url('./" + url + "')"};
+  background-size:100% 100%;
+  ${({width, height}) => `width:${width}; height:${height};`};
 `
 
 const HeaderTitle = styled.div`
   flex:1;
-  background-color: green;
+  display: flex;
+  align-items: center;
+  font-size:18px;
+  font-weight:600;
+  color: white;
 `
 
 const Information = styled.div`
-  width: 300px;
-  background-color: red;
+  display: flex;
+  align-items: center;
+  min-width: 400px;
+  justify-content: flex-end;
+  padding:15px;
+`
+
+const Greeting = styled.div`
+  display:flex;
+  font-size:16px;
+  font-weight:500;
+  color: white;
+  margin-right:30px;
+  align-items:center;
+`
+
+const UserRole = styled.div`
+  display:flex;
+  border-radius: 20px;
+  background-color:#a29bfe;
+  font-size:13px;
+  font-weight:400;
+  padding: 3px 10px;
+  align-items:center; 
+`
+
+const RoleIcon = styled.div`
+  background: url("./user.svg");
+  background-size:100% 100%;
+  width: 20px;
+  height: 20px;
+  margin-right: 10px;
+`
+
+
+const GreetingMessage = styled.div`
+  font-size:16px;
+  font-weight:500;
+  color: white;
+  margin-right:30px;
+  margin-left:10px;
+`
+
+const CircleButton = styled.div`
+  border-radius: 50px;
+  background-color: #0984e3;
+  height:50px;
+  width:50px;
+  display: flex;
+  justify-content: center;
+  align-items:center;
+  font-size:13px;
+  font-weight:400;
+  color: white;
 `
 
 const SBody = styled.div`
-  background-color:yellow;
+  background-color:white;
   height:100%;
   display: flex;
 `
@@ -52,26 +117,50 @@ const MainTitle = styled.div`
 const MainContent = styled.div`
 `
 
+const Divider = styled.div`
+  margin-right: 10px;
+`
+
 const SideBar = styled.div`
-  background-color: grey;
   min-width: 300px;
   flex: 1;
+  background-color: white;
+  border-right: 1px solid #ddd;
 `
 
 const MenuList = styled.div`
   display: flex;
   flex-direction: column;
-  background-color:black;
+  margin-top:10px;
+  margin-left:10px;
 `
 
-const MenuHeader = styled.div`
-  height: 50px;
-  background-color:red;
+const MenuHeader = styled.div`  
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+  
+  font-size:16px;
+  font-weight:700;
+  color: #00cec9;
+`
+
+const IconContainer = styled.div`  
+  width: 100px;
+  display: flex;
+  justify-content: ${({isItem}) => `${isItem && "center"}`}; 
 `
 
 const MenuItem = styled.div`
+  display: flex;
+  padding-left: 10px;
   height: 50px;
-  background-color:blue;
+  
+  font-size:15px;
+  font-weight:500;
+  color: black;
+  align-items:center;
 `
 
 const SMain = styled.div`
@@ -82,34 +171,109 @@ const App = () => {
   return (
     <AppContainer>
       <SHeader>
-        <MenuButton/>
-        <HeaderTitle/>
-        <Information/>
+        <MenuContainer>
+          <IconButton url={"menu-icon.svg"} width="24px" height="24px"/>
+        </MenuContainer>
+        <HeaderTitle>
+          정산 시스템
+        </HeaderTitle>
+        <Information>
+          <Greeting>
+            <UserRole>
+              <RoleIcon/>
+              관리자
+            </UserRole>
+            <GreetingMessage>안운장님 반갑습니다.</GreetingMessage>
+          </Greeting>
+          <CircleButton>정보수정</CircleButton>
+          <Divider/>
+          <CircleButton>로그아웃</CircleButton>
+        </Information>
       </SHeader>
       <SBody>
         <SideBar>
           <MenuList>
-            <MenuHeader/>
-            <MenuItem/>
-            <MenuItem/>
+            <MenuHeader>
+              <IconContainer>
+                <IconButton url={"arrow.svg"} width="15px" height="15px"/>
+              </IconContainer>
+              업주 관리
+            </MenuHeader>
+            <MenuItem>
+              <IconContainer isItem>
+                <IconButton url={"add.svg"} width="15px" height="15px"/>
+              </IconContainer>
+              업주 등록
+            </MenuItem>
+            <MenuItem>
+              <IconContainer isItem>
+                <IconButton url={"search.svg"} width="15px" height="15px"/>
+              </IconContainer>
+              업주 검색
+            </MenuItem>
           </MenuList>
 
           <MenuList>
-            <MenuHeader/>
-            <MenuItem/>
-            <MenuItem/>
+            <MenuHeader>
+              <IconContainer>
+                <IconButton url={"arrow.svg"} width="15px" height="15px"/>
+              </IconContainer>
+              주문 관리
+            </MenuHeader>
+            <MenuItem>
+              <IconContainer isItem>
+                <IconButton url={"add.svg"} width="15px" height="15px"/>
+              </IconContainer>
+              주문 등록
+            </MenuItem>
+            <MenuItem>
+              <IconContainer isItem>
+                <IconButton url={"search.svg"} width="15px" height="15px"/>
+              </IconContainer>
+              주문 검색
+            </MenuItem>
           </MenuList>
 
           <MenuList>
-            <MenuHeader/>
-            <MenuItem/>
-            <MenuItem/>
+            <MenuHeader>
+              <IconContainer>
+                <IconButton url={"arrow.svg"} width="15px" height="15px"/>
+              </IconContainer>
+              보정 금액 관리
+            </MenuHeader>
+            <MenuItem>
+              <IconContainer isItem>
+                <IconButton url={"add.svg"} width="15px" height="15px"/>
+              </IconContainer>
+              보정 금액 등록
+            </MenuItem>
+            <MenuItem>
+              <IconContainer isItem>
+                <IconButton url={"search.svg"} width="15px" height="15px"/>
+              </IconContainer>
+              보정 금액 검색
+            </MenuItem>
           </MenuList>
 
           <MenuList>
-            <MenuHeader/>
-            <MenuItem/>
-            <MenuItem/>
+            <MenuHeader>
+              <IconContainer>
+                <IconButton url={"arrow.svg"} width="15px" height="15px"/>
+              </IconContainer>
+              지급금 관리
+            </MenuHeader>
+            <MenuItem>
+              <IconContainer isItem>
+                <IconButton url={"add.svg"} width="15px" height="15px"/>
+              </IconContainer>
+              지급금 생성/삭제
+            </MenuItem>
+            <MenuItem>
+              <IconContainer isItem>
+                <IconButton url={"search.svg"} width="15px" height="15px"/>
+              </IconContainer>
+              지급금 조회
+            </MenuItem>
           </MenuList>
         </SideBar>
         <SMain>
